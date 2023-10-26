594edbdccd6dd37da4f5bf6ef6d128ed60f7dab4
Justyna Boesche <justyna.boesche@gmail.com>
2023-10-26 12:42:19 +0200

    animation to nav__items added

--- js/main.js
+++ js/main.js
@@ -5,11 +5,23 @@ const allNavItems = document.querySelectorAll('.nav__item');
 const handleNav = () => {
 	nav.classList.toggle('nav--active');
 
-	allNavItems.forEach(item =>
+	allNavItems.forEach(item => {
 		item.addEventListener('click', () => {
 			nav.classList.remove('nav--active');
-		})
-	);
+		});
+	});
+
+	handleNavItemsAnimation();
+};
+
+const handleNavItemsAnimation = () => {
+	let delayTime = 0;
+
+	allNavItems.forEach(item => {
+		item.classList.toggle('nav-items-animation');
+		item.style.animationDelay = '.' + delayTime + 's';
+		delayTime++;
+	});
 };
 
 navBtn.addEventListener('click', handleNav);
