dfc3652b0e3763fd7b3d7f31c87d2c7a095b2e8f
Justyna Boesche <justyna.boesche@gmail.com>
2023-11-06 11:55:49 +0100

    handleCurrentYear function created to show current year in the footer section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const nav = document.querySelector('.nav');
 const navBtn = document.querySelector('.burger-btn');
 const allNavItems = document.querySelectorAll('.nav__item');
+const footerYear = document.querySelector('.footer__year');
 
 const handleNav = () => {
 	nav.classList.toggle('nav--active');
@@ -24,4 +25,10 @@ const handleNavItemsAnimation = () => {
 	});
 };
 
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+
+handleCurrentYear();
 navBtn.addEventListener('click', handleNav);
